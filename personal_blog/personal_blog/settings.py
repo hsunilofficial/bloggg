4e4ec611a8625a8d1b2e0ad1e47544954c9430d9
hsunilofficial <hsunilofficial@gmail.com>
2025-11-19 07:05:27 +0530

    Allow all hosts in settings.py

--- personal_blog/personal_blog/settings.py
+++ personal_blog/personal_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1b908jt^*_h+vvo1i_o*h7fj%3ii-4h8&&qycoj8_ox!$luqj_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
